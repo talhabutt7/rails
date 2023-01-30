8ad82b6b73c6fbf394da903eee5c708d2be42b21
talhabutt7 <talhabuttt@gmail.com>
2023-01-30 21:21:04 +0000

    New routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'hairtapp#index'
 end
